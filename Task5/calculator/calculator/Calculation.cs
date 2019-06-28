9592c0342dc975747332c86e280cde1dce87cf80
jishansiddique <jishans24@gmail.com>
2019-06-28 10:33:07 +0530

    Task 5

---
+++ Task5/calculator/calculator/Calculation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Calculation : ICalculation
+    {
+        public int Sum(List<int> args)
+        {
+            return args.Sum();
+        }
+    }
+}
