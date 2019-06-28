477fde6c33c771e5ebcdd588377871251b35e08e
jishansiddique <jishans24@gmail.com>
2019-06-28 10:33:30 +0530

    Task 6

---
+++ Task6/calculator/calculator/InputStaus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class InputStaus
+    {
+        public bool IsValid { get; set; } = true;
+        public string Message { get; set; }
+    }
+}
